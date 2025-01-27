dcccf3f25a1ddf2545b2cc5fc3ff30ae9b2ca726
GabrielR4SH <bieelsantana23@hotmail.com>
2025-01-27 14:03:35 -0300

    feat: login method done

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\User;
+use Illuminate\Support\Facades\Hash;
 
 class AuthController extends Controller
 {
@@ -15,6 +16,26 @@ class AuthController extends Controller
         ]);
 
         $user = User::create($data);
+        $token = $user->createToken('auth_token')->plainTextToken;
+        return [
+            'user' => $user,
+            'token' => $token
+        ];
+    }
+
+    public function login(Request $request){
+        $data = $request->validate([
+            'email' => ['required','email','exists:users'],
+            'password' => ['required','min:6']
+        ]);
+
+        $user = User::where('email',$data['email'])->first();
+        if(!$user || !Hash::check($data['password'],$user->password)){
+            return response([
+                'message' => 'Not correct',
+            ],401);
+        }
+
         $token = $user->createToken('auth_token')->plainTextToken;
         return [
             'user' => $user,
